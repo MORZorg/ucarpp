a35da39c432c0157021315011bed4d00829ed7fb
Maurizio Zucchelli <mauzuc90@yahoo.it>
2013-03-25 14:27:17 +0100

    Initial Commit

---
+++ ucarpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ucarpp
+//
+//  Created by Maurizio Zucchelli on 03/25/13.
+//  Copyright (c) 2013 Marco Maddiona, Riccardo Orizio, Mattia Rizzini, Maurizio Zucchelli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
